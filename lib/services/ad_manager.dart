639600a22ddacccb0b391069f01d97801ab04081
Jason Bryan <jasonbryan@Ashleys-MBP.lan>
2020-10-27 23:30:34 -0500

    ios support

--- lib/services/ad_manager.dart
+++ lib/services/ad_manager.dart
@@ -4,6 +4,8 @@ class AdManager {
   static String get appId {
     if (Platform.isAndroid) {
       return "ca-app-pub-5316667459256484~9312064451";
+    } else if (Platform.isIOS) {
+      return "ca-app-pub-5316667459256484~5728128381";
     } else {
       throw new UnsupportedError("Unsupported platform");
     }
@@ -12,6 +14,8 @@ class AdManager {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
       return "ca-app-pub-5316667459256484/3601229319";
+    } else if (Platform.isIOS) {
+      return "ca-app-pub-5316667459256484/1911300166";
     } else {
       throw new UnsupportedError("Unsupported platform");
     }
